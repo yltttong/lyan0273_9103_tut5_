5d4ce8c4ba6c55d78fc487dd2f3ec4e4a49ea351
yltttong <1023352840@qq.com>
2023-11-06 22:10:38 +1100

    update

--- sketch.js
+++ sketch.js
@@ -128,14 +128,14 @@ function drawCore(x, y, rad) {
 
 
 function drawDottedLayer(x, y, rad, numLayers, dotsPerLayer) {
-let layerOffset = rad / numLayers;  // 每层之间的距离
-let angleOffset;
-let dotColor = getRandomPastelColor();  // 使用上述函数获取随机色
+  let layerOffset = rad / numLayers;  // Distance between each layer
+  let angleOffset;
+  let dotColor = getRandomPastelColor();  // Get a random color using the above function
+  
+  // Calculate the outermost layer's radius
+  let outerLayerRad = rad + (numLayers - 1) * layerOffset;
 
-// 计算最外层的半径
-let outerLayerRad = rad + (numLayers - 1) * layerOffset;
-
-// 绘制一个统一的圆形背景
+// Draw a uniform circular background
 fill(getRandomPastelColor());
 ellipse(x, y, outerLayerRad * 2);
 noStroke();
@@ -154,114 +154,108 @@ for (let i = 1; i <= numLayers; i++) {
 
 
 function drawRingLayer(x, y, rad, numLayers, ringWidth) {
-let layerOffset = rad / numLayers;  // 每层之间的距离
+  let layerOffset = rad / numLayers;  // Distance between each layer
 
-noStroke();
+  noStroke();
 
-for (let i = numLayers; i >= 1; i--) {  // 注意这里的循环顺序变化
-  let currentRad = rad + (i - 1) * layerOffset;  // 当前层的半径
+  for (let i = numLayers; i >= 1; i--) {  // Note the change in loop order here
+    let currentRad = rad + (i - 1) * layerOffset;  // Radius of the current layer
 
-  fill(getRandomPastelColor());
-  ellipse(x, y, currentRad * 2, currentRad * 2);  // 这里只绘制了一个圆
-}
+    fill(getRandomPastelColor());
+    ellipse(x, y, currentRad * 2, currentRad * 2);  // Only draw one circle here
+  }
 }
 
 
 function drawTriangleStripLayer(x, y, rad, numTriangles) {
-let angleOffset = TWO_PI / numTriangles;  // 每个三角形的角度偏移
+  let angleOffset = TWO_PI / numTriangles;  // Angle offset for each triangle
 
-// 绘制填充无边框的背景圆
-fill(getRandomPastelColor());
-noStroke();
-ellipse(x, y, rad * 2);
+  // Draw a filled, borderless background circle
+  fill(getRandomPastelColor());
+  noStroke();
+  ellipse(x, y, rad * 2);
 
-let innerRad = rad * 0.8;  // 设定小圆的半径
-strokeWeight(1);
-stroke(getRandomPastelColor());
+  let innerRad = rad * 0.8;  // Set the radius of the small circle
+  strokeWeight(1);
+  stroke(getRandomPastelColor());
 
-for (let i = 0; i < TWO_PI; i += angleOffset) {
-  noFill();
+  for (let i = 0; i < TWO_PI; i += angleOffset) {
+    noFill();
 
-  // 圆心
-  let centerX = x;
-  let centerY = y;
+    // Center
+    let centerX = x;
+    let centerY = y;
 
-  // 三角形的第一个顶点，从小圆的边缘开始
-  let firstX = centerX + innerRad * cos(i);
-  let firstY = centerY + innerRad * sin(i);
+    // First vertex of the triangle, starting from the edge of the small circle
+    let firstX = centerX + innerRad * cos(i);
+    let firstY = centerY + innerRad * sin(i);
 
-  // 三角形的第二个顶点，延伸到大圆的边缘
-  let secondX = centerX + rad * cos(i);
-  let secondY = centerY + rad * sin(i);
+    // Second vertex of the triangle, extending to the edge of the large circle
+    let secondX = centerX + rad * cos(i);
+    let secondY = centerY + rad * sin(i);
 
-  // 三角形的第三个顶点，也是从小圆的边缘开始
-  let thirdX = centerX + innerRad * cos(i + angleOffset);
-  let thirdY = centerY + innerRad * sin(i + angleOffset);
+    // Third vertex of the triangle, also starting from the edge of the small circle
+    let thirdX = centerX + innerRad * cos(i + angleOffset);
+    let thirdY = centerY + innerRad * sin(i + angleOffset);
 
-  beginShape();
-  vertex(firstX, firstY);      // 小圆的边缘点
-  vertex(secondX, secondY);    // 大圆的边缘点
-  vertex(thirdX, thirdY);      // 小圆的下一个边缘点
-  endShape(CLOSE);
+    beginShape();
+    vertex(firstX, firstY);      // Edge point of the small circle
+    vertex(secondX, secondY);    // Edge point of the large circle
+    vertex(thirdX, thirdY);      // Next edge point of the small circle
+    endShape(CLOSE);
+  }
 }
-}
-
 
 
 function drawOuterRing(centerX, centerY, maxRadius) {
-let angleOffset = radians(15); // 设置每个元素之间的角度间隔
+  let angleOffset = radians(15); // Set the angle interval between each element
 
-for (let angle = 0; angle < TWO_PI; angle += angleOffset) {
-  //绘制连接线
-  let nextAngle = angle + angleOffset;
-  let startX = centerX + maxRadius * cos(angle);
-  let startY = centerY + maxRadius * sin(angle);
-  let endX = centerX + maxRadius * cos(nextAngle);
-  let endY = centerY + maxRadius * sin(nextAngle);
-  stroke(getRandomPastelColor());
-  strokeWeight(1.5);
-  line(startX, startY, endX, endY);
-  // 绘制黑白橙圆环
-  drawColoredRing(centerX + maxRadius * cos(angle), centerY + maxRadius * sin(angle), maxRadius * 0.05);
+  for (let angle = 0; angle < TWO_PI; angle += angleOffset) {
+    // Draw connecting lines
+    let nextAngle = angle + angleOffset;
+    let startX = centerX + maxRadius * cos(angle);
+    let startY = centerY + maxRadius * sin(angle);
+    let endX = centerX + maxRadius * cos(nextAngle);
+    let endY = centerY + maxRadius * sin(nextAngle);
+    stroke(getRandomPastelColor());
+    strokeWeight(1.5);
+    line(startX, startY, endX, endY);
+    // Draw black-white-orange rings
+    drawColoredRing(centerX + maxRadius * cos(angle), centerY + maxRadius * sin(angle), maxRadius * 0.05);
 
-  // 计算随机颜色椭圆的位置
-  let ellipseX = centerX + maxRadius * cos(angle + angleOffset / 2);
-  let ellipseY = centerY + maxRadius * sin(angle + angleOffset / 2);
+    // Calculate the position of the randomly colored ellipse
+    let ellipseX = centerX + maxRadius * cos(angle + angleOffset / 2);
+    let ellipseY = centerY + maxRadius * sin(angle + angleOffset / 2);
 
-  fill(getRandomPastelColor()); // 使用随机颜色填充椭圆
-  noStroke();
-  ellipse(ellipseX, ellipseY, maxRadius * 0.08, maxRadius * 0.06); // 绘制椭圆
-}
+    fill(getRandomPastelColor()); // Fill the ellipse with a random color
+    noStroke();
+    ellipse(ellipseX, ellipseY, maxRadius * 0.08, maxRadius * 0.06); // Draw the ellipse
+  }
 }
 
 function drawColoredRing(x, y, rad) {
-const colors = ['white', 'black', 'orange'];
-for (let i = 0; i < colors.length; i++) {
-  fill(colors[i]);
-  noStroke();
-  ellipse(x, y, rad * (3 - i), rad * (3 - i));
+  const colors = ['white', 'black', 'orange'];
+  for (let i = 0; i < colors.length; i++) {
+    fill(colors[i]);
+    noStroke();
+    ellipse(x, y, rad * (3 - i), rad * (3 - i));
+  }
 }
-}
-
 
 function drawRandomLine(x, y, rad) {
-noFill();
-stroke(getRandomPastelColor());
-strokeWeight(4);
+  noFill();
+  stroke(getRandomPastelColor());
+  strokeWeight(4);
 
-let angle1 = random(TWO_PI);
-let controlX1 = x + rad * 0.5 * cos(angle1);
-let controlY1 = y + rad * 0.5 * sin(angle1);
-let angle2 = angle1 + random(HALF_PI);
-let controlX2 = x + rad * 0.75 * cos(angle2);
-let controlY2 = y + rad * 0.75 * sin(angle2);
-let angle3 = angle2 + random(HALF_PI);
-let endX = x + rad * cos(angle3);
-let endY = y + rad * sin(angle3);
+  let angle1 = random(TWO_PI);
+  let controlX1 = x + rad * 0.5 * cos(angle1);
+  let controlY1 = y + rad * 0.5 * sin(angle1);
+  let angle2 = angle1 + random(HALF_PI);
+  let controlX2 = x + rad * 0.75 * cos(angle2);
+  let controlY2 = y + rad * 0.75 * sin(angle2);
+  let angle3 = angle2 + random(HALF_PI);
+  let endX = x + rad * cos(angle3);
+  let endY = y + rad * sin(angle3);
 
-bezier(x, y, controlX1, controlY1, controlX2, controlY2, endX, endY);
+  bezier(x, y, controlX1, controlY1, controlX2, controlY2, endX, endY);
 }
-
-
-
-
